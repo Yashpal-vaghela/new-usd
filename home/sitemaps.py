a43e705fd6cd2f09103fdc59041924d757c2f2e6
Yashpal-vaghela <vaghela9632@gmail.com>
2025-05-17 17:24:58 +0530

    dynamic sitemap added

---
+++ home/sitemaps.py
@@ -0,0 +1,57 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+from account.models import *
+
+class HomePageSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+
+    def items(self):
+        return['home:home']
+    def location(self, item):
+        return reverse(item)
+
+class NormalStaticPageSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return['home:contact', 'home:dentist', 'home:gallery']
+    def location(self, item):
+        return reverse(item)
+
+class ListPageSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home:search_all_usd', 'home:blogs']
+    def location(self,item):
+        return reverse(item)
+    
+class BlogPageSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return Blog.objects.all()
+    def location(self, obj):
+        return reverse('home:blogsd', args=[obj.slug])
+
+class CityDentistSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return City.objects.all()
+    def location(self, obj):
+        return reverse('home:search_city_dentists', args=[obj.city])
+    
+class DentistDetailsSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return Dentist.objects.all()
+    def location(self, obj):
+        return reverse('home:find_dentist_d', args=[obj.slug])
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
+from home.sitemaps import *
 from . import views
+
 app_name='home'
+sitemaps_dict ={
+    'home': HomePageSitemap,
+    'static': NormalStaticPageSitemap,
+    'list': ListPageSitemap,
+    'blog': BlogPageSitemap,
+    'citydentist': CityDentistSitemap,
+    'dentistdetails': DentistDetailsSitemap
+}
 urlpatterns = [
     path('', views.home, name='home'),
     # path('find-dentist/', views.find_dentist, name='find_dentist'),
@@ -11,11 +23,13 @@ urlpatterns = [
     path('blogs/<str:pk>/', views.blogsd, name='blogsd'),
     path('contact/', views.contact, name='contact'),
     path('thank-you/', views.thankyou, name='thankyou'),
-    path('sitemap.xml',views.sitemap, name='sitemap'),
+    # path('sitemap.xml/',views.sitemap, name='sitemap'),
     path('robots.txt',views.robots, name='robots'),
     path('certified-dentists/', views.search_all_usd, name='search_all_usd'),
     path('certified-dentists/city/<slug:city_name>/', views.search_city_dentists, name='search_city_dentists'),
-    path('receive_location/', views.receive_location, name='receive_location'),
-    path('locations/', views.location_view, name='locations'),
+    # path('receive_location/', views.receive_location, name='receive_location'),
+    #path('locations/', views.location_view, name='locations'),
+    path('sitemap.xml/', sitemap, {'sitemaps': sitemaps_dict}, name='django.contrib.sitemaps.views.sitemap'),
+
     # path('smile_step/', views.smile_step, name='smile_step'),
 ] 
