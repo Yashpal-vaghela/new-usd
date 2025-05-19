9c65a7644bfd9b241dfd0ed15bf4fd2f0077b0f1
Yashpal-vaghela <vaghela9632@gmail.com>
2025-05-19 09:18:26 +0530

    url updated

--- home/urls.py
+++ home/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('robots.txt',views.robots, name='robots'),
     path('certified-dentists/', views.search_all_usd, name='search_all_usd'),
     path('certified-dentists/city/<slug:city_name>/', views.search_city_dentists, name='search_city_dentists'),
-    # path('receive_location/', views.receive_location, name='receive_location'),
-    #path('locations/', views.location_view, name='locations'),
+    path('receive_location/', views.receive_location, name='receive_location'),
+    path('locations/', views.location_view, name='locations'),
     path('sitemap.xml/', sitemap, {'sitemaps': sitemaps_dict}, name='django.contrib.sitemaps.views.sitemap'),
 
     # path('smile_step/', views.smile_step, name='smile_step'),
