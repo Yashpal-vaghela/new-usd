d7587ca509efd16b112e154b4458dfe867c6f9b0
Yashpal-vaghela <vaghela9632@gmail.com>
2026-04-06 16:00:25 +0530

    update images and export data field

--- virtualsmileAI/admin.py
+++ virtualsmileAI/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import SmileDesignLead
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(SmileDesignLead)
-class SmileDesignLeadAdmin(admin.ModelAdmin):
+class SmileDesignLeadAdmin(ImportExportModelAdmin):
     list_display = ("name", "phone", "city", "created_at")
     search_fields = ("name", "phone", "city")
     list_filter = ('created_at',)
